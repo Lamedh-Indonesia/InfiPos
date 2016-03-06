9b67f52b045cac72e60568d5bd3262203b9d1a02
Dicky Arinal <dicky.arinal@gmail.com>
2016-03-06 17:04:55 +0700

    CRUD in angular.

--- InfiPos.UI.Site.Pos/js/employee.js
+++ InfiPos.UI.Site.Pos/js/employee.js
@@ -9,9 +9,29 @@
         //    { Code: "E03", Name: 'Tidur' },
         //];
 
-        $http.get("http://localhost:4414/api/employee").success(function (data) {
+        var url = "http://localhost:4414/api/employee";
+
+        $http.get(url + '?page=0&size=10').success(function (data) {
             $scope.employees = data;
+            $scope.currentEmployee = $scope.employees[0];
         });
+
+        $scope.selectEmployee = function (employee) {
+            $scope.currentEmployee = employee;
+        }
+
+        $scope.save = function () {
+            $http.post(url, $scope.currentEmployee).success(
+                function () {
+                    alert("Sukses");
+                });
+        }
+
+        $scope.create = function () {
+            $http.get(url + '/0').success(function (data) {
+                $scope.currentEmployee = data;
+            });
+        }
     }
 
     pos.controller("employeeCtrl", employeeCtrl);
